6aed9d82c83b7ec369273b55aafdae8f02b7d880
bryan <15074452001@163.com>
2020-09-03 19:38:27 +0800

    project init3

---
+++ dept_provider_kitty_9201/src/main/java/com/zhc/DeptProviderIktty9201.java
@@ -0,0 +1,15 @@
+package com.zhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bryan
+ * @date 2020/9/3 19:27
+ */
+@SpringBootApplication
+public class DeptProviderIktty9201 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProviderIktty9201.class, args);
+    }
+}
